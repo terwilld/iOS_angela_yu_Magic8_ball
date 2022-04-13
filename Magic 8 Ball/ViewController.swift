9fb78202ded50d7d50863b584f38c8eef43b553b
David Terwilliger <terwilld@gmail.com>
2022-04-13 16:27:35 -0400

    Adjust the view controller a little bit. This is stupid easy but somewhat fun to fix fucking GitHub

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -9,10 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    @IBOutlet weak var imageView: UIImageView!
+   // imageView.image = ballArray[0]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.image = ballArray[0]
 
-
+        
+        
+    }
+    
+    
+    
+    
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        imageView.image = ballArray.randomElement()
+        
+    }
+    
 
 }
 
